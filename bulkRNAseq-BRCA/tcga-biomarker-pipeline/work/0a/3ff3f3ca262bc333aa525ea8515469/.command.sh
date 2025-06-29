5d39acc7017a8033b8f1288e21352d1aa358f654
Aman Kumar <91712601+AKcode08@users.noreply.github.com>
2025-06-29 19:42:34 +0400

    Delete bulkRNAseq-BRCA/tcga-biomarker-pipeline directory

--- bulkRNAseq-BRCA/tcga-biomarker-pipeline/work/0a/3ff3f3ca262bc333aa525ea8515469/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 bin/preprocess.py --input data --output results/preprocessed_data.csv
--- bulkRNAseq-BRCA/tcga-biomarker-pipeline/work/d2/b7a3c40c781d6d0be32cb1380ea292/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python3 /Users/aman/bioinformatics-portfolio/bulkRNAseq-BRCA/workflow/tcga-biomarker-pipeline/workflow/bin/preprocess.py --input data --output results/preprocessed_data.csv
